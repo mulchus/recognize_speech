cb00d4a3e4be4e2be3e0b8c4f2a88d439614ca78
mulchus <mulch@list.ru>
2023-07-27 22:57:25 +0300

    Add echobot

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+import logging
 import os
-
 from dotenv import load_dotenv
+
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from telegram import Update, ForceReply
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
+def start(update: Update, context: CallbackContext):
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+    update.message.reply_text('Здравствуйте')
 
 
-def echo(update, context):
+def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__':
+def main():
     load_dotenv()
-    tg_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    updater = Updater(tg_token)
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    updater = Updater(token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
     updater.start_polling()
     updater.idle()
+
+
+if __name__ == '__main__':
+    main()
