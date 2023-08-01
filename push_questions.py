a59d4a4474532119abdbb7575fa200e2dda82f0b
mulchus <mulch@list.ru>
2023-08-01 17:30:02 +0300

    add DialogFlow learning script (don't work)

---
+++ push_questions.py
@@ -0,0 +1,44 @@
+import json
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def get_questions_from_file():
+    with open('questions.json', "r", encoding="UTF-8") as my_file:
+        questions = json.loads(my_file.read())
+    return questions
+
+
+questions = get_questions_from_file()
+print(questions.values())
+for question in questions.values():
+    print(question)
+    create_intent('mulch-uqka', question['questions'][0], question['questions'], question['answer'])
+
+
