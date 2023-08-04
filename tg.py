2c120466361e17c20c586f2502f7afa09b9e12b2
mulchus <mulch@list.ru>
2023-08-05 00:15:29 +0300

    add_error_handler for tg

--- tg.py
+++ tg.py
@@ -1,25 +1,21 @@
 import os
-import time
 import functions
 
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
-from telegram.error import NetworkError, TelegramError
 
 
 def answer(update: Update, context: CallbackContext):
     project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
     telegram_user_id = os.getenv('TELEGRAM_USER_ID')
-    logger = functions.set_logger()
     intent_text, _ = functions.detect_intent_text(project_id, telegram_user_id, update.message.text, 'ru')
-    try:
-        update.message.reply_text(intent_text)
-    except (ConnectionError, NetworkError, TelegramError) as error:
-        logger.error(f'Потеря или ошибка соединения Телеграм-бота. {error}')
-        time.sleep(5)
-    except Exception as err:
-        logger.error(f'Телеграм-бот упал с ошибкой: {err}')
+    update.message.reply_text(intent_text)
+
+
+def error_handler(update, context):
+    logger = functions.set_logger()
+    logger.error(msg='Телеграм-бот упал с ошибкой:', exc_info=context.error)
 
 
 def main():
@@ -28,6 +24,7 @@ def main():
     updater = Updater(telegram_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
+    dispatcher.add_error_handler(error_handler)
     updater.start_polling()
     updater.idle()
 
