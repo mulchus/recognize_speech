1272a8cc6db71da82883f63d072bfe85f6d2b8e0
mulchus <mulch@list.ru>
2023-08-04 22:10:40 +0300

    Deleted main and restructured other scripts

--- tg.py
+++ tg.py
@@ -1,32 +1,32 @@
+import os
 import time
-import vk
-from run import detect_intent_texts
-from telegram import Update, Bot
+import functions
+
+from dotenv import load_dotenv
+from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
-
-from main import telegram_token, telegram_user_id, project_id
-
-bot = Bot(telegram_token)
-
-
-def send_message(message):
-    bot.send_message(telegram_user_id, str(message))
+from telegram.error import NetworkError, TelegramError
 
 
 def answer(update: Update, context: CallbackContext):
-    intent_texts, is_fallback = detect_intent_texts(project_id, telegram_user_id, (update.message.text, ), 'ru')
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    telegram_user_id = os.getenv('TELEGRAM_USER_ID')
+    logger = functions.set_logger()
+    intent_texts, is_fallback = functions.detect_intent_texts(project_id, telegram_user_id, (update.message.text, ), 'ru')
     if not is_fallback:
         intent_texts = [x.encode('utf-8').decode() for x in intent_texts]
         try:
             update.message.reply_text(''.join(intent_texts))
-        except (ConnectionError, telegram.error.NetworkError, telegram.error.TelegramError) as error:
-            vk.send_message(f'Потеря или ошибка соединения Телеграм-бота. {error}')
+        except (ConnectionError, NetworkError, TelegramError) as error:
+            logger.error(f'Потеря или ошибка соединения Телеграм-бота. {error}')
             time.sleep(5)
         except Exception as err:
-            vk.send_message(f'Телеграм-бот упал с ошибкой: {err}')
+            logger.error(f'Телеграм-бот упал с ошибкой: {err}')
 
 
 def main():
+    load_dotenv()
+    telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
     updater = Updater(telegram_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
--- vk.py
+++ vk.py
@@ -1,23 +1,12 @@
+import os
 import random
 import urllib
 import vk_api as vk
+import functions
+
+from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
-
-import tg
-from main import vk_token, vk_user_id, project_id
-from run import detect_intent_texts
 from urllib.error import HTTPError
-
-vk_session = vk.VkApi(token=vk_token)
-vk_api = vk_session.get_api()
-
-
-def send_message(message):
-    vk_api.messages.send(
-        user_id=vk_user_id,
-        message=str(message),
-        random_id=random.randint(1, 1000)
-    )
 
 
 def check_vk_request_error(response):
@@ -35,8 +24,8 @@ def check_vk_request_error(response):
         )
 
 
-def answer(event, vk_api):
-    intent_texts, is_fallback = detect_intent_texts(project_id, event.user_id, (event.text, ), 'ru')
+def answer(event, vk_api, project_id):
+    intent_texts, is_fallback = functions.detect_intent_texts(project_id, event.user_id, (event.text, ), 'ru')
     if not is_fallback:
         intent_texts = [x.encode('utf-8').decode() for x in intent_texts]
         response = vk_api.messages.send(
@@ -48,13 +37,19 @@ def answer(event, vk_api):
 
 
 def main():
+    load_dotenv()
+    logger = functions.set_logger()
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    vk_token = os.getenv('VK_TOKEN')
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             try:
-                answer(event, vk_api)
+                answer(event, vk_api, project_id)
             except urllib.error.HTTPError as error:
-                tg.send_message(f'{error} {error.url}')
+                logger.error(f'{error} {error.url}')
 
 
 if __name__ == '__main__':
