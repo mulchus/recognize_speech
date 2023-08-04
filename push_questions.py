cd3aa8aab87a8b2b07b4707cbd2dd80943554c9b
mulchus <mulch@list.ru>
2023-08-04 22:12:12 +0300

    Added def main

--- push_questions.py
+++ push_questions.py
@@ -1,17 +1,15 @@
+import os
 import json
 from google.cloud import dialogflow
 from google.api_core.exceptions import InvalidArgument
-from main import project_id
 
 
 def create_intent(project_id, display_name, training_phrases_parts, message_texts):
-    """Create an intent of the given intent type."""
     intents_client = dialogflow.IntentsClient()
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
-        # Here we create a new training phrase for each provided part.
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
 
@@ -35,11 +33,17 @@ def get_questions_from_file():
     return questions
 
 
-questions_array = get_questions_from_file()
-for question_subject, questions in questions_array.items():
-    if len(questions['answer']) > 300:
-        questions['answer'] = questions['answer'][0:300]
-    try:
-        create_intent(project_id, question_subject, questions['questions'], [questions['answer']])
-    except InvalidArgument:
-        pass
+def main():
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    questions_array = get_questions_from_file()
+    for question_subject, questions in questions_array.items():
+        if len(questions['answer']) > 300:
+            questions['answer'] = questions['answer'][0:300]
+        try:
+            create_intent(project_id, question_subject, questions['questions'], [questions['answer']])
+        except InvalidArgument:
+            pass
+
+
+if __name__ == '__main__':
+    main()
